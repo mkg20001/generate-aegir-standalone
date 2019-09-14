7f1ab5c976504f725961f488abb1ffa179278477
Maciej Krüger <mkg20001@gmail.com>
2019-09-14 21:04:58 +0200

    feat: update

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMP=$(mktemp -d)
+git clone git@github.com:mkg20001/eslint-config-aegir-standalone "$TMP"
+mv "$TMP/.git" "out"
+rm -rf "$TMP"
+
+VER=$(cat ./aegir/package.json | jq -r .version)
+
+cd out
+git add .
+git commit -m "Update to $VER"
+git tag "v$VER"
+git push --tags
+git push
+
+
